1d8687db38939207ce166bce3a47140a52ef894b
Tin Vo <tinnvo1101@gmail.com>
2016-04-03 11:42:14 -0500

    Added hide tests output button

--- app/static/js/about.js
+++ app/static/js/about.js
@@ -2,10 +2,10 @@ var app = angular.module('about', []);
 app.controller('aboutCtrl', function($scope, $http) {
 	$scope.runTests = function() {
 		$scope.showTestsOutput = true;
-		$scope.testOutput = 'Running Tests ... '
+		$scope.testOutput = '\nRunning Tests ... '
 		$http.get('/run_unittests').then(function(result){
-			$scope.testOutput = result.data.output;
-			console.log(result.data.output);
+			$scope.finished = true;
+			$scope.testOutput = '\n' + result.data.output;
 		});
 	}
 });
