11ea8d5d9efc1adbd3835c10d2e18a632a1ce3af
Tin Vo <tinnvo1101@gmail.com>
2016-04-17 15:00:36 -0500

    Hide authors in books' section

--- app/ggmate/static/js/app.js
+++ app/ggmate/static/js/app.js
@@ -364,15 +364,15 @@ mainApp.controller('booksCtrl', function($scope, $http) {
 
     $http.get('/books').success(function(res) {
         $scope.books = res.books;
-        console.log(res);
     });
 
     $http.get('/authors').success(function(res) {
         $scope.authors = res.authors;
-        console.log(res);
     });
 
-    jQuery('.authors').hide();
+    jQuery(document).ready(function() {
+        jQuery('.authors').hide();
+    });  
 });
 
 mainApp.controller('ScrollCtrl', function($scope, $location, anchorSmoothScroll) {
